b75f7ad5ec7e74193fb2ff0d4f8d22d2adea6ebe
cristhian Gael <al03065581@tecmilenio.mx>
2024-01-26 14:20:30 -0600

    Este es el codigo completo del reto 1

--- src/edu/gael_rivera/reto1/process/CalculadoraMasa.java
+++ src/edu/gael_rivera/reto1/process/CalculadoraMasa.java
@@ -15,6 +15,13 @@ public class CalculadoraMasa {
      * @return indice de masa corporal
      */
     public static double calcularMasaCorporal(double altura, double peso){
+        /**
+         *este metodo nos indica que a traves de un if y else if
+         * ponemos los casos posibles u condiciones a cumplir
+         * para calcular el indice masa corporal, ademas que en la
+         * formula especificamos que las variales se resolveran matematicamente
+         * a traves de Math.pow
+         */
         double metros = altura /100;
         double imc = peso/Math.pow(metros,2);
         if(imc <= 18.50){
@@ -37,7 +44,23 @@ public class CalculadoraMasa {
         }
         return imc;
     }
-    public static double masaMagra(double peso, double altura, char sexo){
+
+    /**
+     * Este metodo sirve para calcular la masa magra de una
+     * persona con base a su sexo, peso, altura
+     * @param sexo sexo del paciente si es masculino "m || M" o femenino "f || F"
+     * @param peso peso del paciente en kg
+     * @param altura estatura del paciente en centimetros
+     * @return masa magra
+     */
+    public static double masaMagra(char sexo, double peso, double altura){
+        /**
+         * Este metodo nos muestra que a traves de nuestros datos ya
+         * guardados como lo es el peso y la altura, ahora solo pedimos
+         * al usuario que ingrese su sexo y atraves de un if y else if
+         * dependiendo del caso hacemos las operaciones con Math.pow
+         * para asi saber cual es la masa magra dependiendo del sexo del usuario
+         */
         double masaMagra = 0;
         if (sexo == 'f' || sexo == 'F'){
             masaMagra = 1.07*peso-148*Math.pow(peso, 2)/Math.pow(altura, 2);
@@ -48,5 +71,35 @@ public class CalculadoraMasa {
             System.out.println("Error al ingresar sexo");
         }
 
+        return masaMagra;
+    }
+
+    /**
+     * Este metodo sirve para calcular el metabolismo basal de una
+     * persona con base a su sexo, peso, altura y edad
+     * @param sexo sexo del paciente si es masculino "m || M" o femenino "f || F"
+     * @param peso peso del paciente en kg
+     * @param altura estatura del paciente en centimetros
+     * @param edad pide la edad del usuario en un valor entero
+     * @return metabolismo basal
+     */
+    public static double metabolismoBasal(char sexo, double peso, double altura, int edad){
+        /**
+         * En este metodo usamos solo operaciones basicas para dar el
+         * resultado solicitado del usuarios, usamos el if y else if para calcular
+         * las condicion es o casos a especificar
+         */
+        double metabolismoBasal = 0;
+        if (sexo == 'f' || sexo == 'F'){
+            metabolismoBasal = 655.1 + (9.563 * peso)+(1.85 * altura)-(4.676 * edad);
+
+        } else if (sexo == 'm' || sexo == 'M') {
+            metabolismoBasal = 66.5 + (13.75 * peso)+(5.003 * altura)-(6.775 * edad);
+
+        }else {
+            System.out.println("Error al ingresar sexo");
+        }
+
+        return metabolismoBasal;
     }
 }
--- src/edu/gael_rivera/reto1/ui/CLI.java
+++ src/edu/gael_rivera/reto1/ui/CLI.java
@@ -5,7 +5,15 @@ import edu.gael_rivera.reto1.process.CalculadoraMasa;
 import java.util.Scanner;
 
 public class CLI {
+    /**
+     * En esta clase es para toda la interaccion con el usuario.
+     */
     public static void showMenu(){
+        /**
+         * En este metodo le damos el menu al usuario, enumeramos
+         * las opciones del 1 al 4 y el usuario tendra que seleccionar
+         * a sus necesidades requeridas.
+         */
         System.out.println("Bienvenido");
         System.out.println("Escribe el numero de la opcion que desees");
         System.out.println("1. Indice de masa corporal");
@@ -15,6 +23,17 @@ public class CLI {
         System.out.println("*********************");
     }
     public static void launchApp(){
+        /**
+         * En este metodo interactuamos con el usuario si selecciona opcion 1 le pedimos
+         * que ingrese su estatura a centimetros, su peso a kg y ya obtenido los datos
+         * se podra ejecutar la calculadora del indice de masa corporal.
+         * Si selecciona la opcion 2 le pedimos al usuario que ingrese lo mismo que ahora tambien
+         * ingrese su sexo con una sola letra ya se a "M o F" dependiendo del sexo.
+         * Si selecciona la opcion 3 pasa lo mismo y ademas del sexo le pedimos que ingrese
+         * su edad en enteros.
+         * Si selecciona la opcion 4 se finlalizara el programa.
+         * Al seleccionar cualquiera de las opcciones el programa sae ejecutara en base a lo esperado.
+         */
         Scanner sc = new Scanner(System.in);
         showMenu();
         int seleccion = 0;
@@ -26,9 +45,30 @@ public class CLI {
             double peso = sc.nextDouble();
             double imc = CalculadoraMasa.calcularMasaCorporal(altura, peso);
             System.out.println("Tu IMC es: "+ imc);
-        }else if(seleccion == 2);{
+        }else if(seleccion == 2){
+            System.out.println("Ingresa tu sexo");
             char sexo = sc.next().charAt(0);
+            System.out.println("Ingrese peso en kilogramos");
+            double peso = sc.nextDouble();
+            System.out.println("Ingrese altura en centimetros");
+            double altura = sc.nextDouble();
+            double masaMasaMagra = CalculadoraMasa.masaMagra(sexo, peso, altura);
+            System.out.println("Tu masa magra es: "+ masaMasaMagra);
 
+        }else if(seleccion == 3){
+            System.out.println("Ingresa tu sexo");
+            char sexo = sc.next().charAt(0);
+            System.out.println("Ingresa el peso en kilogramos");
+            double peso = sc.nextDouble();
+            System.out.println("Ingresa la altura en centimetros");
+            double altura = sc.nextDouble();
+            System.out.println("Ingresa tu edad");
+            int edad = sc.nextInt();
+            double metabolismoBasal = CalculadoraMasa.metabolismoBasal(sexo, peso, altura, edad);
+            System.out.println("Tu metabolismo basal es: "+ metabolismoBasal);
+        } else if (seleccion == 4) {
+            System.out.println("Finalizo el programa, Gracias por su atención");
+            System.exit(0);
         }
     }
 }
